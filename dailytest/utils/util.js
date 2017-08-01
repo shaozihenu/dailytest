9d4136d4e21fe55e9e131287dabf936fc21eb3da
shaozihenu <shaozihenu@qq.com>
2017-08-01 09:52:31 +0800

    bug修改
    日期bug修改

--- dailytest/utils/util.js
+++ dailytest/utils/util.js
@@ -16,10 +16,12 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
-function dateSubtractDate(date) {
+function dateSubtractDate(date1) {
+  var date=new Date(date1)
+  date.setDate(date.getDate()-1)
   var year = date.getFullYear()
   var month = date.getMonth() + 1
-  var day = date.getDate()-1
+  var day = date.getDate()
 
   var hour = 23
   var minute = 59
